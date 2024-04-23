a4e2246ed2dd80929f6aeb26c9eb2bc9759d9538
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-04-23 11:17:21 -0300

    Subo carpeta de Etapa 0

---
+++ Etapa0/1_medrano_HolaMundo/1_medrano_HolaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_medrano_HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre; 
+            Console.WriteLine("¿cual es tu nombre?");
+            nombre=Console.ReadLine();
+            Console.WriteLine("hola," + nombre);
+            Console.ReadKey();
+        }
+    }
+}
