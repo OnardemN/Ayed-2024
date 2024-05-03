848eba30e7bfb77d7467cc41fbf625f9fe87e73d
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-05-03 19:22:30 -0300

    Subo carpeta de Etapa 1

---
+++ Etapa 1/11_medrano_ParImpar/11_medrano_ParImpar/11_medrano_ParImpar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("ingresa un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            int numero2 = 2;
+
+            if (numero % numero2 == 0)
+            {
+                Console.WriteLine("es par");
+            }
+
+            else
+            {
+                Console.WriteLine("es impar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 1/12_medrano_CobrarBeca/12_medrano_CobrarBeca/12_medrano_CobrarBeca/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("ingresa tu edad");
+            edad = int.Parse(Console.ReadLine());
+
+            int saldo;
+            Console.WriteLine("e ingresa tus ingresos mensuales");
+            saldo = int.Parse(Console.ReadLine());
+
+            if (edad>=19 && saldo>=100000)
+            {
+                Console.WriteLine("podes cobrar");
+            }
+            else
+            {
+                Console.WriteLine("no podes cobrar");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
