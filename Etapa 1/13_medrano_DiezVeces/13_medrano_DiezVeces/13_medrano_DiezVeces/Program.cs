fb76f079e8b4659172a62d7441097ecb81bf1d8d
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-05-03 19:24:51 -0300

    Subo carpeta de Etapa 1

---
+++ Etapa 1/13_medrano_DiezVeces/13_medrano_DiezVeces/13_medrano_DiezVeces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            Console.WriteLine("ingrese una palabra");
+            palabra = Console.ReadLine();
+            int contador = 0;
+
+            while (contador < 10)
+            {
+                Console.WriteLine("palabra: " + (palabra));
+                contador++;
+                
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 1/14_medrano_Cumplidos/14_medrano_Cumplidos/14_medrano_Cumplidos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+            while (100 >= contador)
+            {
+
+                if (contador % 3 == 0 && contador % 5 == 0)
+                {
+                    Console.WriteLine((contador) + " buzzfizz");
+                }
+                else if (contador % 5 == 0)
+                {
+                    Console.WriteLine((contador) + " buzz");
+                }
+
+                else if (contador % 3 == 0)
+                {
+                    Console.WriteLine((contador) + " fizz");
+                }
+
+                else
+                {
+                    Console.WriteLine(contador);
+                }
+                contador++;
+
+
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
