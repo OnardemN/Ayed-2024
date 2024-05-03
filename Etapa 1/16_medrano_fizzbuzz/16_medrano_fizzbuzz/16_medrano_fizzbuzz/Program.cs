fab354735c9faa65def0369edda38329befcdbf7
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-05-03 19:29:42 -0300

    Subo carpeta de Etapa 1

---
+++ Etapa 1/16_medrano_fizzbuzz/16_medrano_fizzbuzz/16_medrano_fizzbuzz/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_medrano_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribe un programa que muestre por consola (con un print) los  números de 1 a 100 (ambos incluidos y con un salto de línea entre  cada impresión), sustituyendo los siguientes: 
+ - Múltiplos de 3 por la palabra "fizz". 
+ - Múltiplos de 5 por la palabra "buzz". 
+ - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".*/
+
+            int num;
+            num = 0;
+
+            while (num < 100)
+            {
+                num++;
+
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (num%3!=0 && num%5!=0)
+                {
+                    Console.WriteLine(num);
+                }
+
+                if (num % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+            }
+
+            Console.ReadKey();
+
+
+             
+        }
+    }
+}
---
+++ Etapa 1/18_medrano_Hastaquesi/18_medrano_HastaQueSi/18_medrano_HastaQueSi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_medrano_HastaQueSi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escriba un programa que pregunte una y otra vez si desea terminar el programa, salvo si se contesta exactamente SI (en mayúsculas y sin tilde).*/
+            String pregunta;
+            pregunta = "";
+
+            while  (pregunta != "SI")
+            {
+                Console.WriteLine("desea terminar con el programa ");
+                pregunta = Console.ReadLine();
+
+               
+            }
+            Console.WriteLine("hasta luego");
+            Console.ReadKey();
+          
+     
+            {
+
+            }
+        }
+    }
+}
