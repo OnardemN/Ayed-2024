1e0eaab6761202607723a555a40923bc5cdd78b2
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-05-07 15:03:58 -0300

    Subo carpeta de la Etapa 1

---
+++ Etapa 1/19_medrano_HataN/19_medrano_HastaN/19_medrano_HastaN/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_medrano_HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Recorre los números del 1 al N. (N lo tiene que ingresar el usuario).
+              Muestra solamente los números pares*/
+            int N;
+            Console.WriteLine("ingres un numero ");
+            N = int.Parse(Console.ReadLine());
+
+            int contador;
+            contador = 1;
+
+            while (N>contador )
+            {
+                contador++;
+
+                if (contador % 2 == 0)
+                {
+                    Console.WriteLine(contador);
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
