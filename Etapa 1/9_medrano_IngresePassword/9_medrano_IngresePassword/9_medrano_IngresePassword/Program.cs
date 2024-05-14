41f3da1b43667417d8c8f068ad24f6ddd3137a37
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-05-14 14:45:46 -0300

    subo carpeta de etapa 1

---
+++ Etapa 1/9_medrano_IngresePassword/9_medrano_IngresePassword/9_medrano_IngresePassword/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseña;
+            contraseña = "nayeli2807";
+            Console.WriteLine("ingresa tu contraseña");
+
+            string contraseña2;
+            contraseña2 = Console.ReadLine();
+
+
+
+            if (contraseña2 == contraseña)
+            {
+                Console.WriteLine("es correcta");
+            }
+            else
+            {
+                Console.WriteLine("no es correcta");
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
