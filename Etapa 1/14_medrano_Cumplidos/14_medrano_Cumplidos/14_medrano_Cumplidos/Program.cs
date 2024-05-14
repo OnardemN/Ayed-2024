973c77fdae64eb52324ea6c2fd681ac13f226bea
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-05-14 14:48:20 -0300

    subo carpeta de etapa 1

---
+++ Etapa 1/14_medrano_Cumplidos/14_medrano_Cumplidos/14_medrano_Cumplidos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int edad;
+            Console.WriteLine("¿Cuál es tu edad?");
+            edad = int.Parse(Console.ReadLine());
+
+            int contador = 1;
+
+            while (contador <= edad)
+            {
+                Console.WriteLine("cumplido " + contador);
+                contador++;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
