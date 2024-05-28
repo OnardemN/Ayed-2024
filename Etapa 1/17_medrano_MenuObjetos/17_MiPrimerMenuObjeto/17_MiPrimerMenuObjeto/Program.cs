adc9c07129af2ff6e937c5093fa29f16b52eceb8
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-05-28 16:55:33 -0300

    Subo carpeta de Etapa 1

---
+++ Etapa 1/17_medrano_MenuObjetos/17_MiPrimerMenuObjeto/17_MiPrimerMenuObjeto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_MiPrimerMenuObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Salir = false;
+            string opcion;
+            Console.WriteLine("Bienvenido a nuestro programa, te dejo el menu, elige un numero de las opciones");
+            Console.WriteLine("---Menu---");
+            Console.WriteLine("1.opcion.1");
+            Console.WriteLine("2.opcion.2");
+            Console.WriteLine("3.opcion.3");
+            Console.WriteLine("4.salir");
+            while (!Salir)
+            {
+                Console.WriteLine("Seleccione una opcion: ");
+                opcion = Console.ReadLine();
+                if (opcion == "1")
+                {
+                    Console.WriteLine("HAMBURGESA:BACON CHEDDAR McMELT");
+                }
+                else if (opcion == "2")
+
+                {
+                    Console.WriteLine("McFIESTA");
+                }
+                else if (opcion == "3")
+                {
+                    Console.WriteLine("TRIPLE HAMBURGESA CON QUESO ");
+                }
+                else if (opcion == "4")
+                {
+                    Console.WriteLine("¡Hasta luego!");
+                    Salir = true;
+                }
+                else
+                {
+                    Console.WriteLine("Opcion no valida");
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
