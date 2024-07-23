258ba3ae4642bbc61bc0aba0502e65b7e08584c2
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-07-22 22:14:16 -0300

    Subo la Carpeta de la Etapa 2

---
+++ Etapa2/5_medrano_HotSale/5-medrano_HotSale/5-medrano_HotSale/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int productos;
+Console.WriteLine("ingrese la cantidad de productos vendidos");
+productos = int.Parse(Console.ReadLine());
+int[] cantproductos = new int[productos];
+
+for(int i =0; i<productos; i++)
+{
+    Console.WriteLine("ingrese el precio de cada producto" + (i +  1));
+    cantproductos[i] += int.Parse(Console.ReadLine());
+
+}
+
+for (int i = 0; i < cantproductos.Count() - 1; i++)
+{
+    for (int j=0; j < cantproductos.Count() - 1 - i; j++)
+    {
+        if (cantproductos[j] < cantproductos[j + 1])
+        {
+            int aux = cantproductos[j];
+            cantproductos[j] = cantproductos[j + 1];
+            cantproductos[j + 1] = aux;
+        }
+    }
+}
+
+Console.WriteLine("el producto mas carro es " + cantproductos[0]);
+Console.WriteLine("el producto mas varato es " + cantproductos[productos - 1]);
+
+Console.ReadKey();
+
