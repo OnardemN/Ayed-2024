228c5c0abcc3313464ab40ffe5aac2f1d86aaa36
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-07-22 23:42:11 -0300

    Subo la Carpeta de la Etapa 2

---
+++ Etapa2/6_medrano_ZEGARPG/6-medrano_ZEGARPG/6-medrano_ZEGARPG/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int objetos;
+Console.WriteLine("ingrese la cantidad de objetos ");
+objetos = int.Parse(Console.ReadLine());
+int[] cantobjetos = new int[objetos];
+
+int valor;
+Console.WriteLine("ingrese el valor minimo");
+valor = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < objetos; i++)
+{
+    Console.WriteLine("ingre los valores de los objetos n  " + (i + 1));
+    cantobjetos[i] += int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < objetos; i++)
+{
+    if (cantobjetos[i] >= valor)
+    {
+        Console.WriteLine("posiscion   " + (i+1)+ ":" + cantobjetos[i]);
+    }
+}
+
+Console.ReadKey();
+
