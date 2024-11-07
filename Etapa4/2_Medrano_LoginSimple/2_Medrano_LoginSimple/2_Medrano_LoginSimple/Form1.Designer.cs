af89700304ced35de5755c79bac20231f9f32e67
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-11-07 16:29:55 -0300

    Subo la Carpeta de la Etapa 4

---
+++ Etapa4/2_Medrano_LoginSimple/2_Medrano_LoginSimple/2_Medrano_LoginSimple/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace _2_Medrano_LoginSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.boton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese nombre del usuario";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(268, 77);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(100, 20);
+            this.nombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ingrese contraseña";
+            // 
+            // contraseña
+            // 
+            this.contraseña.Location = new System.Drawing.Point(274, 212);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(100, 20);
+            this.contraseña.TabIndex = 3;
+            // 
+            // boton
+            // 
+            this.boton.Location = new System.Drawing.Point(296, 270);
+            this.boton.Name = "boton";
+            this.boton.Size = new System.Drawing.Size(75, 23);
+            this.boton.TabIndex = 4;
+            this.boton.Text = "acceder";
+            this.boton.UseVisualStyleBackColor = true;
+            this.boton.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 325);
+            this.Controls.Add(this.boton);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.Button boton;
+    }
+}
+
---
+++ Etapa4/2_Medrano_LoginSimple/2_Medrano_LoginSimple/2_Medrano_LoginSimple/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_Medrano_LoginSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void boton_Click(object sender, EventArgs e)
+        {
+            if (nombre.Text == "Admin" && contraseña.Text == "Admin12345")
+            {
+                MessageBox.Show("ingreso correctamente");
+            }
+            else if (nombre.Text == "Admin" && contraseña.Text != "Admin12345") 
+            {
+                MessageBox.Show("contraseña incorrectamente");
+            }
+            else if (nombre.Text != "Andim" && contraseña.Text == "Admin12345")
+            {
+                MessageBox.Show("usuario incorrectamente");
+            }
+            else
+            {
+                MessageBox.Show("usuario y contraseña incorrectamente");
+            }
+
+            
+        }
+    }
+}
