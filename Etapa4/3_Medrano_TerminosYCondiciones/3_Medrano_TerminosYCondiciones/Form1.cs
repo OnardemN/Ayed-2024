67312f604ae759441826fa476ade835e968d2fe2
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-11-07 14:25:43 -0300

    Subo la Carpeta de la Etapa 4

---
+++ Etapa4/3_Medrano_TerminosYCondiciones/3_Medrano_TerminosYCondiciones/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_Medrano_TerminosYCondiciones
+{
+    public partial class acep : Form
+    {
+        public acep()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aceptar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (aceptar.Checked == true)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+    }
+}
