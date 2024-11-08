e6633285a25dd2d59933d0ae8cbe6d7391b9bed7
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-11-08 18:05:41 -0300

    Subo la Carpeta de la Etapa 3

---
+++ Etapa3/0_Medrano_MiPrimerFuncion/0_Medrano_MiPrimerFuncion/0_Medrano_MiPrimerFuncion/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+
+{
+    static string n(string x)
+    {
+        x = "Hola " + x;
+        return x;
+    }
+    static void Main(string[] args)
+    {
+        string nombre;
+        Console.WriteLine("Como te llamas?");
+        nombre = Console.ReadLine();
+        Console.WriteLine(n(nombre));
+
+        Console.ReadKey();
+    }
+}
---
+++ Etapa3/2_Medrano_AreaVolumen/2_Medrano_AreaVolumen/2_Medrano_AreaVolumen/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace _2_Medrano_AreaVolumen
+{
+    class Program
+    {
+        static double area(double x)
+        {
+            double pi = 3.14;
+            x = pi * (x * x);
+            return x;
+        }
+        static double volumen(double y, double x)
+        {
+            y = area(y) * x;
+            return y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el radio del circulo:");
+            double radio = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el radio del cilindro:");
+            double radiocili = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del cilindro:");
+            double altura = double.Parse(Console.ReadLine());
+            Console.WriteLine(volumen(radiocili, altura));
+            Console.ReadKey();
+
+
+        }
+    }
+
+}
