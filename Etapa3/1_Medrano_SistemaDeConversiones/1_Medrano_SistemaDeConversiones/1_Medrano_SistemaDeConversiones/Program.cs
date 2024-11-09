0c52743192b5c9bbaca205e07f0f4fc16bd82cf7
OnardemN <167466732+OnardemN@users.noreply.github.com>
2024-11-08 23:07:13 -0300

    Subo la Carpeta de la Etapa 3

---
+++ Etapa3/1_Medrano_SistemaDeConversiones/1_Medrano_SistemaDeConversiones/1_Medrano_SistemaDeConversiones/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Medrano_SistemaDeConversiones
+{
+    class Program
+    {
+
+        static string deci(int x)
+        {
+            string bin = " ";
+            while (x > 0)
+            {
+                bin = (x % 2) + bin;
+                x /= 2;
+            }
+            return bin;
+        }
+        static int binario(string x)
+        {
+            return Convert.ToInt32(x, 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero para pasarlo a binario:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("En binario es: " + (deci(num)));
+            Console.ReadKey();
+            Console.WriteLine("Ingrese un numero en binario");
+            string bin1 = Console.ReadLine();
+            Console.WriteLine("En decimal es :"+binario(bin1));
+            Console.ReadKey();
+        }
+    }
+}
